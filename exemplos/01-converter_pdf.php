fb743fc8c2bc28ef0568f433e0fd69a241245ffd
renatoolegario <73908159+renatoolegario@users.noreply.github.com>
2022-11-08 09:06:13 -0300

    At

--- exemplos/01-converter_pdf.php
+++ exemplos/01-converter_pdf.php
@@ -6,7 +6,7 @@ include("../bibliotecas/01-converter_pdf/autoload.php");
 
 				use Dompdf\Dompdf;
                 $dompdf = new Dompdf();				
-                $html = file_get_contents('exemplo_html_para_pdf.html');
+                $html = file_get_contents('01-exemplo_html_para_pdf.html');
                 $dompdf->loadHtml($html);
                 $dompdf->setPaper('A4');
                 $dompdf->render();
